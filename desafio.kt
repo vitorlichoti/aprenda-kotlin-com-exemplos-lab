b88d203cfe1dd0338719315e2416540c4ac962ff
Vitor Lichoti <vitorlichoti@gmail.com>
2023-05-22 22:54:50 -0400

    solução desafio kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,24 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+   
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val formacao = Formacao("TQI", listOf(ConteudoEducacional("Java Kotlin", Nivel.BASICO, 80)))
+    formacao.matricular(Usuario("Vitor"))
+    formacao.matricular(Usuario("Luis"))
+    formacao.matricular(Usuario("Hugo"))
+   
+    println(formacao.inscritos)
+    println(formacao.conteudos)
 }
